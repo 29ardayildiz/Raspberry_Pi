b4121b2bb8eaed3b66bdaa0be4654a7ef141bbc2
Arda YILDIZ <89103146+29ardayildiz@users.noreply.github.com>
2025-06-02 19:31:36 +0300

    Update Raspberry_Pi_System_Monitoring.py

--- Raspberry_Pi_System_Monitoring.py
+++ Raspberry_Pi_System_Monitoring.py
@@ -180,7 +180,7 @@ def dashboard():
                                 {{ info.temp }} °C
                             </div>
                             <div class="progress">
-                                
+                                {% if info.temp != "N/A" %}
                                 <div class="progress-bar bg-danger" role="progressbar" 
                                      style="width: {{ (info.temp / 85 * 100) if info.temp <= 85 else 100 }}%">
                                 </div>
